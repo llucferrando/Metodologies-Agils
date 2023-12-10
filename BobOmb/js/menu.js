25079a6670d49340266b7707c4265816e7e263ba
Pau Insa <pau.insa@enti.cat>
2023-12-10 17:43:59 +0100

    Scene navigation

--- BobOmb/js/menu.js
+++ BobOmb/js/menu.js
@@ -31,7 +31,7 @@ menuNavigation()
       this.play_button.on("pointerdown", () => {
           this.cameras.main.fadeOut(2000, 0, 0, 0);
           this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
-              this.scene.start('gameState')
+              this.scene.start('level1')
               
           })
       })
